7ddad55a9a182cf1b7c08b5a7e26488e8c76708d
Dpk <dpkcareergreatness@gmail.com>
2024-12-13 23:31:34 +0000

    day11 reference p2 solution

---
+++ day11/day11_p2_referencesolution.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+
+from functools import lru_cache
+DATA = './day11.txt'
+# DATA = 'testdata202411.txt'
+
+
+def GetData(datafile):
+  """Read input into a list of integers."""
+  lines = []
+  with open(datafile, 'r') as fh:
+    lines = [i.strip() for i in fh]
+  assert len(lines) == 1
+  nums = [int(i) for i in lines[0].split()]
+  return nums
+
+
+@lru_cache
+def Blink(number):
+  """Return a list of new number(s), given a single number."""
+  if number == 0:
+    return [1]
+  if not len(str(number)) % 2:
+    str_num = str(number)
+    half = len(str_num)//2
+    return [int(str_num[:half]), int(str_num[half:])]
+  return [number * 2024]
+
+
+@lru_cache(maxsize=None)
+def BlinkSingleLen(num, rounds):
+  """Return the length of the list after n rounds, starting with
+     a single number (not a list)."""
+  if rounds == 0:
+    return 1
+  if rounds == 1:
+    return len(Blink(num))
+
+  return sum([BlinkSingleLen(n, rounds - 1) for n in Blink(num)])
+
+
+def Solver(nums, rounds):
+  """Solver for both parts, pass in the requisite number of rounds."""
+  total = 0
+  for i in nums:
+    subtotal = BlinkSingleLen(i, rounds)
+    print(f'    {i} -> {subtotal}')
+    total += subtotal
+  return total
+
+
+def main():
+  """main"""
+  nums = GetData(DATA)
+  print(f'Part 2: {Solver(nums, 75)}')
+
+
+if __name__ == '__main__':
+  main()
